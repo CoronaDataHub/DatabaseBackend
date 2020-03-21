dd9b83a62f2b69cf91cd7ef004ca41ba46853bc1
Julian Haag <newspicel11@gmail.com>
2020-03-21 19:30:19 +0100

    Add RestAPI

---
+++ src/main/java/de/coronadatahub/databasebackend/restapi/RestAPI.java
@@ -0,0 +1,25 @@
+/*
+ * *
+ *    ____                                          ____            _             _   _           _
+ *   / ___|   ___    _ __    ___    _ __     __ _  |  _ \    __ _  | |_    __ _  | | | |  _   _  | |__
+ *  | |      / _ \  | '__|  / _ \  | '_ \   / _` | | | | |  / _` | | __|  / _` | | |_| | | | | | | '_ \
+ *  | |___  | (_) | | |    | (_) | | | | | | (_| | | |_| | | (_| | | |_  | (_| | |  _  | | |_| | | |_) |
+ *   \____|  \___/  |_|     \___/  |_| |_|  \__,_| |____/   \__,_|  \__|  \__,_| |_| |_|  \__,_| |_.__/
+ *
+ *  	CoronaDataHub ist ein Projekt welches im Rahmen von der Initiative #WirVSVirus-Hackathon vom 20-22 März 2020 ins Leben gerufen wurde.
+ *
+ *
+ */
+
+package de.coronadatahub.databasebackend.restapi;
+
+import static spark.Spark.*;
+
+public class RestAPI {
+
+    private void init(){
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+
+}
